356bbaae6abdb88f6fa24d7b0b4d3e0786b1698b
ZarKljh <zarkljh@gmail.com>
2025-11-03 16:13:55 +0900

    ㅑãinit

---
+++ src/test/java/com/ll/jpa2511/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.jpa2511;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
